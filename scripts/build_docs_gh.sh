bca0b72400034d6f74075eb8ae6fbf7d166463d2
Jens A. Koch <jakoch@web.de>
2021-03-08 20:52:17 +0100

    ooof. move multiline to script

---
+++ scripts/build_docs_gh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TAGS=$(<../versions.txt)
+
+for TAG in ${TAGS}
+do
+  echo "Checking out version: $TAG"
+
+  git checkout -q $TAG
+
+  if [[ $TAG == v* ]];
+  then
+    VER="${TAG:1}"
+  else
+    VER=$TAG
+  fi
+
+  mkdir -p "../docs/ttauri/${VER}"
+
+  echo "Setting environment vars for doxygen config:"
+  export TTAURI_TAG="${TAG}"
+  export TTAURI_VERSION="${VER}"
+
+  echo "TTAURI_TAG     -> $TTAURI_TAG"
+  echo "TTAURI_VERSION -> $TTAURI_VERSION"
+
+  echo "Current working dir: $PWD"
+
+  doxygen ../scripts/Doxyfile
+
+done
